f2237e2cc9343f06f4a29d8b981fbe4d7ef2431f
kimenyu <njorogekimenyu@gmail.com>
2023-06-19 14:06:59 +0000

    setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,6 @@
---creating a databse called hbnb_dev_db
+-- Creates database hbnb_dev_db, user hbnb_dev and grants priveleges
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---creating a new user 
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
---hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
---hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
